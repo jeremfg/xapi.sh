890349df4131b88ab829a0433eae502b6f0acba2
Jérémie Faucher-Goulet <JFaucherGoulet@orthogone.com>
2025-02-08 03:47:30 +0000

    Add support for enabling USB passthrough

--- src/passthrough.sh
+++ src/passthrough.sh
@@ -11,7 +11,7 @@
 #   0: Success
 #   1: Failure
 #   2: Reboot required
-passthrough_configure() {
+passthrough_pci_configure() {
   local pciback_arg
 
   
@@ -43,6 +43,194 @@ passthrough_configure() {
     return 2
   fi
 }
+
+# Configure USB devices to they can be passed though to a VM
+#
+# Parameters:
+#   $@: USB device names to passthrough
+# Returns:
+#   0: Success
+#   1: Failure
+passthrough_usb_configure () {
+  local usb var_usb_vid var_usb_pid var_usb_sn scan_performed
+  scan_performed=0
+
+  # Make sure we have all the data we need
+  for usb in "${@}"; do
+    var_usb_vid="UDEV_VID_${usb}"
+    var_usb_pid="UDEV_PID_${usb}"
+    var_usb_sn="UDEV_SN_${usb}"
+
+    if [[ -z "${!var_usb_vid}" ]]; then
+      logError "Missing VID for ${usb}"
+      return 1
+    elif [[ -z "${!var_usb_pid}" ]]; then
+      logError "Missing PID for ${usb}"
+      return 1
+    elif [[ -z "${!var_usb_sn}" ]]; then
+      logError "Missing SN for ${usb}"
+      return 1
+    fi
+  done
+
+  # Load XE utilities
+  # shellcheck disable=SC1091
+  if ! source "${PI_ROOT}/src/xe_host.sh"; then
+    logError "Failed to load xe_host.sh"
+    return 1
+  fi
+  
+  local vid pid sn usb_output
+  for usb in "${@}"; do
+    var_usb_vid="UDEV_VID_${usb}"
+    var_usb_pid="UDEV_PID_${usb}"
+    var_usb_sn="UDEV_SN_${usb}"
+    vid="${!var_usb_vid}"
+    pid="${!var_usb_pid}"
+    sn="${!var_usb_sn}"
+
+    # Find out if the USB device is plugged in
+    if ! usb_output=$(lsusb -d "${vid}:${pid}"); then
+      logError "Failed to list USB devices with VID:PID ${vid}:${pid}"
+      return 1
+    elif [[ -z "${usb_output}" ]]; then
+      logError "USB device not found: ${usb}"
+      return 1
+    fi
+
+    # Iterate over each line of lsusb to find the correct device
+    local line bus device udevadm_output cur_sn res
+    cur_sn=""
+    while IFS= read -r line; do
+      # Extract bus and device number
+      bus=$(echo "${line}" | cut -d' ' -f2)
+      device=$(echo "${line}" | cut -d' ' -f4 | cut -d':' -f1 || true)
+      if [[ -z "${bus}" ]] || [[ -z "${device}" ]]; then
+        logError "Failed to extract bus and device number"
+        return 1
+      fi
+
+      # Get the serial number
+      if ! udevadm_output=$(udevadm info --query=all "--name=/dev/bus/usb/${bus}/${device}"); then
+        logError "Failed to get udevadm info for ${bus}:${device}"
+        return 1
+      elif [[ -z "${udevadm_output}" ]]; then
+        logError "udevadm info not found for ${bus}:${device}"
+        return 1
+      fi
+
+      cur_sn=$(echo "${udevadm_output}" | grep "ID_SERIAL_SHORT=" | cut -d'=' -f2 || true)
+      if [[ -z "${cur_sn}" ]]; then
+        logError "Failed to extract serial number for ${bus}:${device}"
+        return 1
+      fi
+
+      if [[ "${cur_sn}" == "${sn}" ]]; then
+        logInfo "Found USB device: ${usb}"
+        break
+      else
+        logWarn "Found a similar USB device to ${usb}, but with serial number ${cur_sn}"
+        cur_sn=""
+      fi
+    done <<< "${usb_output}"
+
+    if [[ -z "${cur_sn}" ]]; then
+      logError "Failed to find USB device: ${usb}"
+      return 1
+    fi
+
+    logInfo <<EOF
+USB device ${usb} found:
+  VID   : ${vid}
+  PID   : ${pid}
+  SN    : ${sn}
+  Bus   : ${bus}
+  Device: ${device}
+EOF
+
+    # Check if Xen sees the USB device
+    while true; do
+      local xen_usb_output
+      if ! xe_exec xen_usb_output pusb-list "vendor-id=${vid}" "product-id=${pid}" "serial=${sn}"; then
+        logError "Failed to list USB devices in Xen"
+        return 1
+      elif [[ -n "${xen_usb_output}" ]]; then
+        logInfo "USB device is visible to Xen: ${usb}"
+        break
+      else
+        logWarn "USB device is not visible to Xen: ${usb}"
+      fi
+
+      # If we reach here, the USB device is not visible to Xen.
+      # Try to configure USB policies if not already configured
+      local policy
+      policy="ALLOW:vid=${vid} pid=${pid} # Edited by scripts"
+      if ! grep -q "${policy}" "${USB_POLICY_FILE}"; then
+        logInfo "USB policy not found for ${usb}, adding it"
+        # Add policy as the first line that is not a comment
+        if ! sed -i "/^[^#]/i ${policy}" "${USB_POLICY_FILE}"; then
+          logError "Failed to add USB policy for ${usb}"
+          return 1
+        else
+          scan_performed=0
+          logTrace "USB policy added for ${usb}"
+        fi
+      else
+        logInfo "USB policy found for ${usb}"
+        if [[ ${scan_performed} -eq 1 ]]; then
+          logError "Unable to list USB device in xen: ${usb}"
+          return 1
+        else
+          logInfo "Scanning for USB devices in Xen"
+          if ! xe_host_current res; then
+            logError "Failed to get current host"
+            return 1
+          elif ! xe_exec res pusb-scan "host-uuid=${res}"; then
+            logError "Failed to scan for USB devices in Xen"
+            return 1
+          else
+            scan_performed=1
+          fi
+        fi
+      fi
+    done
+
+    # If we reach here, the USB device is visible to Xen
+    # Make sure passthrough is enabled for it
+    local usb_uuid p_enabled
+    if ! xe_exec usb_uuid pusb-list "vendor-id=${vid}" "product-id=${pid}" "serial=${sn}" --minimal; then
+      logError "Failed to get USB UUID"
+      return 1
+    elif [[ -z "${usb_uuid}" ]]; then
+      logError "USB not found: ${usb}"
+      return 1
+    fi
+
+    if ! xe_exec p_enabled pusb-param-get "uuid=${usb_uuid}" "param-name=passthrough-enabled"; then
+      logError "Failed to get passthrough-enabled"
+      return 1
+    elif [[ "${p_enabled}" == "true" ]]; then
+      logInfo "Passthrough already enabled for ${usb}"
+    elif [[ "${p_enabled}" == "false" ]]; then
+      logInfo "Enabling passthrough for ${usb}"
+
+      if ! xe_exec res pusb-param-set "uuid=${usb_uuid}" "passthrough-enabled=true"; then
+        logError "Failed to enable passthrough for ${usb}"
+        return 1
+      fi
+    else
+      logError "Unknown value for passthrough-enabled: ${p_enabled}"
+      return 1
+    fi
+    # If we reach here, all is good for our device, move on to the next one
+  done
+
+  # If we reach here, it means all USB devices are configured properly
+  return 0
+}
+
+# Constants
+USB_POLICY_FILE="/etc/xensource/usb-policy.conf"
 
 ###########################
 ###### Startup logic ######
